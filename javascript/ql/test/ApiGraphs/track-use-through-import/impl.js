fb102f693d214a18fafa03c62a5fdcd8e576d5d3
Max Schaefer <max-schaefer@github.com>
2020-10-12 10:09:44 +0100

    JavaScript: Improve forward-tracking of use nodes.

---
+++ javascript/ql/test/ApiGraphs/track-use-through-import/impl.js
@@ -0,0 +1,2 @@
+const mod = require(".");
+mod.foo = 0; /* def (member foo (member exports (module track-use-through-import))) */
---
+++ javascript/ql/test/ApiGraphs/track-use-through-import/index.js
@@ -0,0 +1,1 @@
+exports.bar = 1; /* def (member bar (member exports (module track-use-through-import))) */
